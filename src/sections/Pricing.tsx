d70c0d832e90de504670d587485ae1496732101c
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-24 20:07:14 -0300

    Pricing | Cards content styles added

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,3 +1,5 @@
+import CheckIcon from "@/assets/check.svg"
+
 const pricingTiers = [
   {
     title: "Free",
@@ -70,16 +72,26 @@ export const Pricing = () => {
               inverse,
               features,
             }) => (
-              <div key={title}>
-                <h3>{title}</h3>
-                <div>
-                  <span>${monthlyPrice}</span>
-                  <span>/month</span>
+              <div
+                key={title}
+                className="rounded-3xl border border-[#f1f1f1] p-10 shadow-[0px_7px_14px_#eaeaea]"
+              >
+                <h3 className="text-lg font-bold text-black/50">{title}</h3>
+                <div className="mt-[30px] flex items-baseline gap-1">
+                  <span className="text-4xl font-bold leading-none tracking-tighter">
+                    ${monthlyPrice}
+                  </span>
+                  <span className="font-bold tracking-tight text-black/50">
+                    /month
+                  </span>
                 </div>
-                <button>{buttonText}</button>
-                <ul>
+                <button className="btn btn-primary mt-[30px] w-full">
+                  {buttonText}
+                </button>
+                <ul className="mt-8 flex flex-col gap-5">
                   {features.map((feature, index) => (
-                    <li key={index}>
+                    <li key={index} className="flex items-center gap-4 text-sm">
+                      <CheckIcon className="size-6" />
                       <span>{feature}</span>
                     </li>
                   ))}
