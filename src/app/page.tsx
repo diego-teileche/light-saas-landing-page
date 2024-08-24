91724b93884fde3932fbf46489fc13b9b71564e1
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-24 20:56:01 -0300

    Testimonials
    - Tag added
    - Title added
    - Paragraph added
    - Basic styles added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { Hero } from "@/sections/Hero"
 import { LogoTicker } from "@/sections/LogoTicker"
 import { Pricing } from "@/sections/Pricing"
 import { ProductShowcase } from "@/sections/ProductShowcase"
+import { Testimonials } from "@/sections/Testimonials"
 
 export default function Home() {
   useTitleChanger({
@@ -19,6 +20,7 @@ export default function Home() {
       <LogoTicker />
       <ProductShowcase />
       <Pricing />
+      <Testimonials />
     </>
   )
 }
--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,12 +1,12 @@
-import avatar1 from "@/assets/avatar-1.png";
-import avatar2 from "@/assets/avatar-2.png";
-import avatar3 from "@/assets/avatar-3.png";
-import avatar4 from "@/assets/avatar-4.png";
-import avatar5 from "@/assets/avatar-5.png";
-import avatar6 from "@/assets/avatar-6.png";
-import avatar7 from "@/assets/avatar-7.png";
-import avatar8 from "@/assets/avatar-8.png";
-import avatar9 from "@/assets/avatar-9.png";
+import avatar1 from "@/assets/avatar-1.png"
+import avatar2 from "@/assets/avatar-2.png"
+import avatar3 from "@/assets/avatar-3.png"
+import avatar4 from "@/assets/avatar-4.png"
+import avatar5 from "@/assets/avatar-5.png"
+import avatar6 from "@/assets/avatar-6.png"
+import avatar7 from "@/assets/avatar-7.png"
+import avatar8 from "@/assets/avatar-8.png"
+import avatar9 from "@/assets/avatar-9.png"
 
 const testimonials = [
   {
@@ -63,8 +63,21 @@ const testimonials = [
     name: "Casey Harper",
     username: "@casey09",
   },
-];
+]
 
 export const Testimonials = () => {
-  return null;
-};
+  return (
+    <section className="bg-white">
+      <div className="container">
+        <div className="flex justify-center">
+          <div className="tag">Testimonials</div>
+        </div>
+        <h2 className="section-title mt-5">What our users say</h2>
+        <p className="section-description mt-5">
+          From intuitive design to powerful features, our app has become an
+          essential tool for users around the world
+        </p>
+      </div>
+    </section>
+  )
+}
