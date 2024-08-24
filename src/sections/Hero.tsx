f73116f2e599b0d9be2241194474e8da73ca9001
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-24 16:32:10 -0300

    Hero | Content
    - Texts added
    - Title added
    - Paragraph added
    - Buttons added
    - Some styles added

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,24 @@
 export const Hero = () => {
-  return <section>Diego</section>
+  return (
+    <section>
+      <div className="container">
+        <div>
+          <div className="inline-flex rounded-lg border border-[#222]/10 px-3 py-1 text-sm tracking-tight">
+            Version 2.0 is here
+          </div>
+          <h1 className="text-5xl font-bold tracking-tighter">
+            Pathway to productivity
+          </h1>
+          <p>
+            Celebrate the joy of accomplishment with an app designed to track
+            your progress, motivate your efforts, and celebrate your successes
+          </p>
+          <div>
+            <button>Get for free</button>
+            <button>Learn more</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
