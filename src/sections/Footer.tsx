41aef26c0683c8f21fbc003f103087372e3b0d87
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-25 19:27:45 -0300

    Footer | Styles and Responsive Design added

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -13,7 +13,7 @@ export const Footer = () => {
         <div className="relative inline-flex before:absolute before:bottom-0 before:top-2 before:w-full before:bg-[linear-gradient(to_right,#f87bff,#fb92cf,#ffdd9b,#c2f0b1,#2fd8fe)] before:blur before:content-['']">
           <Image src={logo} height={40} className="relative" alt="SaaS Logo" />
         </div>
-        <nav>
+        <nav className="mt-6 flex flex-col gap-6 md:flex-row md:justify-center">
           <a href="#">About</a>
           <a href="#">Features</a>
           <a href="#">Customers</a>
@@ -21,14 +21,16 @@ export const Footer = () => {
           <a href="#">Help</a>
           <a href="#">Careers</a>
         </nav>
-        <div>
+        <div className="mt-6 flex justify-center gap-6">
           <SocialX />
           <SocialInsta />
           <SocialLinkedIn />
           <SocialPin />
           <SocialYoutube />
         </div>
-        <p>&copy; 2024 Your Company, Inc. All rights reserved.</p>
+        <p className="mt-6">
+          &copy; 2024 Your Company, Inc. All rights reserved.
+        </p>
       </div>
     </footer>
   )
