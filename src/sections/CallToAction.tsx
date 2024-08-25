a1abd22e8aa549a3fc34c291115908cbb22ed392
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-25 18:33:21 -0300

    Call to Action | Basic Styles
    - Section styles added
    - Title styles added
    - Paragraph styles added
    - Buttons styles added

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,15 +1,15 @@
 export const CallToAction = () => {
   return (
-    <section>
+    <section className="bg-gradient-to-b from-white to-[#d2dcff] py-24">
       <div className="container">
-        <h2>Sign up for free today</h2>
-        <p>
+        <h2 className="section-title">Sign up for free today</h2>
+        <p className="section-description mt-5">
           Celebrate the joy of accomplishment with an app designed to track your
           progress and motivate your efforts
         </p>
-        <div>
-          <button>Get for free</button>
-          <button>Learn more</button>
+        <div className="mt-10 flex justify-center gap-2">
+          <button className="btn btn-primary">Get for free</button>
+          <button className="btn btn-text">Learn more</button>
         </div>
       </div>
     </section>
